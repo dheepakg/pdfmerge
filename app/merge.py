33fc27429d2798ae707b24531f766a8594cd7f7b
dheepakg <sslvgdek@gmail.com>
2020-04-18 11:19:43 +0530

    init build

--- app/merge.py
+++ app/merge.py
@@ -1,6 +1,25 @@
-print("test")
+import argparse
 
 
 def read_files():
-    print("fghjk")
-    return {1: "fghjk", 2: "rtyui"}
+    """
+    Reads files from CLI
+    """
+    try:
+        input_args = argparse.ArgumentParser(
+            description="Reads pdf files (mandatory). Also, optional args"
+        )
+        input_args.add_argument("--version", action="version", version="%(prog)s 1.0")
+        input_args.add_argument("File1", metavar="Key in first file", action="store")
+        input_args.add_argument("File2", metavar="Key in second file", action="store")
+
+        my_namespace = input_args.parse_args()
+        return my_namespace
+    except:
+        return False
+
+
+if __name__ == "__main__":
+    arguments = read_files()
+    print(arguments.File1)
+    print(arguments.File2)
